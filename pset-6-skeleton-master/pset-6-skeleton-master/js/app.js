06cf2db7835a97cb358d8d3fe1cdf537192d3eda
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2020-01-05 01:46:50 -0500

    4

--- pset-6-skeleton-master/pset-6-skeleton-master/js/app.js
+++ pset-6-skeleton-master/pset-6-skeleton-master/js/app.js
@@ -5,3 +5,13 @@ let priority_button = document.getElementsByClassName("priority_button");
 let complete_button = document.getElementsByClassName("complete_button");
 let remove_button = document.getElementsByClassName("remove_button");
 let list_item = document.getElementsByClassName("to_do")
+
+let element_prioritize;
+
+window.onload = function() {
+  document.getElementById("table").onmouseover = startup;
+}
+
+const create_item = function() {
+  let input = document.getElementById("input_item").value;
+  if (input === "") {
