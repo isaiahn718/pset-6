6aa1aa83d3c16369d93a203fca618070e8a117d0
isaiahn718 <55455558+isaiahn718@users.noreply.github.com>
2020-01-05 00:52:07 -0500

    3

---
+++ pset-6-skeleton-master/pset-6-skeleton-master/js/app.js
@@ -0,0 +1,7 @@
+let elements = []
+var add = document.getElementById("add_button");
+var row = document.getElementsByClassName("row");
+let priority_button = document.getElementsByClassName("priority_button");
+let complete_button = document.getElementsByClassName("complete_button");
+let remove_button = document.getElementsByClassName("remove_button");
+let list_item = document.getElementsByClassName("to_do")
